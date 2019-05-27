a2f761c0e76bdbd107033a296e66eaf2127b4c89
Mira Han <mira.han@unlv.edu>
2019-05-27 16:16:30 -0700

    added commands file for example command lines

--- script/dedup.sh
+++ script/dedup.sh
@@ -8,33 +8,33 @@ echo $FQBASE1
 echo $FQBASE2
 echo $DIRNAME
 
-#awk 'NR % 4 == 1' ${FQFILE1} > ${DIRNAME}/1.IDs &
-#awk 'NR % 4 == 1' ${FQFILE2} > ${DIRNAME}/2.IDs &
+awk 'NR % 4 == 1' ${FQFILE1} > ${DIRNAME}/1.IDs &
+awk 'NR % 4 == 1' ${FQFILE2} > ${DIRNAME}/2.IDs &
 
-#grep '\/2$' ${DIRNAME}/1.IDs > ${DIRNAME}/2in1.IDs
-#grep '\/1$' ${DIRNAME}/2.IDs > ${DIRNAME}/1in2.IDs
+grep '\/2$' ${DIRNAME}/1.IDs > ${DIRNAME}/2in1.IDs
+grep '\/1$' ${DIRNAME}/2.IDs > ${DIRNAME}/1in2.IDs
 
-#grep -A 3 -F -f ${DIRNAME}/2in1.IDs ${FQFILE1} >> ${FQFILE2}
-#grep -A 3 -F -f ${DIRNAME}/1in2.IDs ${FQFILE2} >> ${FQFILE1}
+grep -A 3 -F -f ${DIRNAME}/2in1.IDs ${FQFILE1} >> ${FQFILE2}
+grep -A 3 -F -f ${DIRNAME}/1in2.IDs ${FQFILE2} >> ${FQFILE1}
 
-#grep -v '\/2$' ${DIRNAME}/1.IDs  > ${DIRNAME}/1.clean.IDs
-#cat ${DIRNAME}/1in2.IDs  >> ${DIRNAME}/1.clean.IDs
-#grep -v '\/1$' ${DIRNAME}/2.IDs  > ${DIRNAME}/2.clean.IDs
-#cat ${DIRNAME}/2in1.IDs  >> ${DIRNAME}/2.clean.IDs
+grep -v '\/2$' ${DIRNAME}/1.IDs  > ${DIRNAME}/1.clean.IDs
+cat ${DIRNAME}/1in2.IDs  >> ${DIRNAME}/1.clean.IDs
+grep -v '\/1$' ${DIRNAME}/2.IDs  > ${DIRNAME}/2.clean.IDs
+cat ${DIRNAME}/2in1.IDs  >> ${DIRNAME}/2.clean.IDs
 
-#sed 's/\//\t/' ${DIRNAME}/1.clean.IDs | sort -k 1,1 |  uniq > ${DIRNAME}/1.uniq.IDs
-#sed 's/\//\t/' ${DIRNAME}/2.clean.IDs | sort -k 1,1 |  uniq > ${DIRNAME}/2.uniq.IDs
+sed 's/\//\t/' ${DIRNAME}/1.clean.IDs | sort -k 1,1 |  uniq > ${DIRNAME}/1.uniq.IDs
+sed 's/\//\t/' ${DIRNAME}/2.clean.IDs | sort -k 1,1 |  uniq > ${DIRNAME}/2.uniq.IDs
 
-#join ${DIRNAME}/1.uniq.IDs ${DIRNAME}/2.uniq.IDs > ${DIRNAME}/common.IDs
-#awk '{print $1"/1"}' ${DIRNAME}/common.IDs > ${DIRNAME}/1.common.IDs &
-#pid=$!
-#awk '{print $1"/2"}' ${DIRNAME}/common.IDs > ${DIRNAME}/2.common.IDs 
-#wait $pid
+join ${DIRNAME}/1.uniq.IDs ${DIRNAME}/2.uniq.IDs > ${DIRNAME}/common.IDs
+awk '{print $1"/1"}' ${DIRNAME}/common.IDs > ${DIRNAME}/1.common.IDs &
+pid=$!
+awk '{print $1"/2"}' ${DIRNAME}/common.IDs > ${DIRNAME}/2.common.IDs 
+wait $pid
 
-#split -C 300m --numeric-suffixes ${DIRNAME}/1.common.IDs ${DIRNAME}/split.1.common.IDs. &
-#pid=$!
-#split -C 300m --numeric-suffixes ${DIRNAME}/2.common.IDs ${DIRNAME}/split.2.common.IDs.
-#wait $pid
+split -C 300m --numeric-suffixes ${DIRNAME}/1.common.IDs ${DIRNAME}/split.1.common.IDs. &
+pid=$!
+split -C 300m --numeric-suffixes ${DIRNAME}/2.common.IDs ${DIRNAME}/split.2.common.IDs.
+wait $pid
 
 
 for file in ${DIRNAME}/split.1.*
@@ -54,5 +54,5 @@ for pid in ${pids[*]}; do
     wait $pid
 done
 
-cat split.1.*.fastq > ${FQFILE1}.dedup.fastq
-cat split.2.*.fastq > ${FQFILE2}.dedup.fastq
+cat ${DIRNAME}/split.1.*.fastq > ${FQFILE1}.dedup.fastq
+cat ${DIRNAME}/split.2.*.fastq > ${FQFILE2}.dedup.fastq
